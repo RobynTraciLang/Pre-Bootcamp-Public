fb7809aadfafe06d5575958c40d286e04c374890
RobynTraciLang <124897590+RobynTraciLang@users.noreply.github.com>
2023-04-24 21:03:52 -0500

    Practicing JS with HTML and CSS!

---
+++ onmouseover and onmouseleave practice/script.js
@@ -0,0 +1,54 @@
+function changeContent(element) {
+    element.innerText = "Hey Hey Hey!"
+    console.log(element.innerText)
+}
+
+function changeBack(element) {
+    element.innerText = "Hello World!"
+    console.log(element.innerText)
+}
+
+// function changeContent(element) {
+//     element.innerText = "My full name is Robyn Traci Lang."
+//     console.log(element.innerText)
+// }
+
+// function changeBack(element) {
+//     element.innerText = "My name is Robyn."
+//     console.log(element.innerText)
+// }
+
+// function changeContent(element) {
+//     element.innerText = "Right now I'm a Grants Accountant!"
+//     console.log(element.innerText)
+// }
+
+// function changeBack(element) {
+//     element.innerText = "I have been many things."
+//     console.log(element.innerText)
+// }
+
+// function changeContent(element) {
+//     element.innerText = "See ya later!"
+//     console.log(element.innerText)
+// }
+
+// function changeBack(element) {
+//     element.innerText = "Thanks for chatting!"
+//     console.log(element.innerText)
+// }
+
+// setTimeout(changeBack, 3000)
+
+
+// function changeContent(element){
+//     if(element.innerText = "Hello World!"){
+//         console.log(element.innerText)
+//         element.innerText = "Hey Hey Hey!"
+//     }
+//     else if(element.innerText = "Hey Hey Hey!"){
+//         console.log(element.innerText)
+//         element.innerText = "Hello World!"
+//     }
+// }
+// How do I make this last function work?
